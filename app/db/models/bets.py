bc9d95eabb30e5e466298c3776f3cc894b9dfe59
Федотов Дмитрий Вадимович <fedotovdv@mosreg.ru>
2024-03-19 02:45:20 +0300

    added db

---
+++ app/db/models/bets.py
@@ -0,0 +1,18 @@
+import uuid
+
+from sqlalchemy import Column, DateTime, Integer, String, Float
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class Bets(Base):
+    __tablename__ = 'bets'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    event_id = Column(Integer)
+    bet_sum = Column(Float)
+    status = Column(String)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
