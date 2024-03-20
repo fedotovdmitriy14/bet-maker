8bf278d3030f5a4ef8e797732c641e86a8cc35a5
Федотов Дмитрий Вадимович <fedotovdv@mosreg.ru>
2024-03-20 11:48:40 +0300

    more tests

--- tests/test_bets.py
+++ tests/test_bets.py
@@ -10,8 +10,19 @@ def test_make_new_bet(client):
 
 
 def test_get_all_bets(client, set_up_and_tear_down_bets):
-    response = client.post(
+    response = client.get(
         f'api/v1/bets',
     )
     assert response.status_code == 200
     assert len(response.json()) == 5
+
+
+def test_get_all_bets_pagination(client, set_up_and_tear_down_bets):
+    response = client.get(
+        f'api/v1/bets',
+        params={
+            'page_size': 2
+        }
+    )
+    assert response.status_code == 200
+    assert len(response.json()) == 2
